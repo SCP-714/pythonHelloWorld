7575e2a3fb32e77632590862391f76154947b18c
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-04 12:24:21 -0500

    prompts user 2 questions

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,6 @@
 print('helloWorld');
+bain = 'bain';
+print(bain)
+name = input('What is your pet’s name?');
+activity =input('What is his/her favorite activity?');
+print("Your pets name is {}.".format(name) + 'their favorite thing to do is {}.'.format(activity));
