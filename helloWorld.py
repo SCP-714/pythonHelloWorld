e1b5cf30d96ab04cb34226f2e0eacdcc091aac79
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-06 21:44:38 -0500

    HelloWorld Dr. Kurt Diesch

--- helloWorld.py
+++ helloWorld.py
@@ -3,4 +3,5 @@ print('Hello Dr. Kurt Diesch');
 
 name = input('What is your pet’s name?');
 activity =input('What is his/her favorite activity?');
+
 print("Your pets name is {}.".format(name) + ' Their favorite thing to do is {}.'.format(activity));
