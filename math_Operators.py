929cf69cd7b3ac702dcd1e9fb37a86266399c8e9
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-22 00:56:14 -0500

    python math operators

---
+++ math_Operators.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.ceil(5.5));
+
+# Initialize the number of items to choose from
+n = 7
+
+# Initialize the number of possibilities to choose
+k = 5
+
+# Print total number of possible combinations
+print (math.comb(n, k))
--- weight_converson.py
+++ weight_converson.py
@@ -3,5 +3,5 @@ weight_lb = input('How much do you weigh in pounds?');
 
 weight_kg =float(weight_lb) * .45;
 
-`
+
 print('You weigh {}'.format(weight_kg) + ' Kilograms, you should hit the gym!');
