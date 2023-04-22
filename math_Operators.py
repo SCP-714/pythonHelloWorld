81b0ec3e4c95ce17a8655a5f1bd70b28f9398511
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-22 01:05:00 -0500

    python math operators

--- math_Operators.py
+++ math_Operators.py
@@ -1,12 +1,18 @@
 import math
 
-print(math.ceil(5.5));
-
+print(math.ceil(5.2));
+print(math.floor(5.6));
 # Initialize the number of items to choose from
 n = 7
-
 # Initialize the number of possibilities to choose
 k = 5
-
 # Print total number of possible combinations
 print (math.comb(n, k))
+
+#Return the value of the first parameter and the sign of the second parameter
+print(math.copysign(4, -1))
+print(math.copysign(-43, -76))
+
+#Print absolute values from numbers
+print(math.fabs(-66.43))
+print(math.fabs(-7))
