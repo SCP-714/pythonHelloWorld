ad318ae93933b296feba49cbe2b3dad2420595af
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-22 19:10:02 -0500

    completd name length counter

--- length.py
+++ length.py
@@ -1,8 +1,8 @@
 name = input('Please tell me your name!')
 
 if len(name) >= 20:
-    print('Your Name is Too Long!')
+    print('Your Name is Too Long! Your name may not be longer than 20 letters!')
 elif len(name) <= 2:
-    print('Your Name is Too Short!')
+    print('Your Name is Too Short! Your name may not be shorter than 2 letters!')
 else:
     print('Your Name is Awesome!')
